2ef846d3b6be4ce3f2276a5d439ca1c607a096bf
Raj Saha <krajsaha@gmail.com>
2026-08-04 22:27:18 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] ans = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            ans[s.charAt(i)-'a']+=1;
+            ans[t.charAt(i)-'a']-=1;
+        }
+
+        for(int i=0;i<26;i++){
+            if(ans[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
