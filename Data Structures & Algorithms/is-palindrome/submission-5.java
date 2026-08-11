d969060ee1009dbc7fbe6582e771b75a4463c863
Raj Saha <krajsaha@gmail.com>
2026-08-11 20:15:08 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int si=0;
+        int ei = s.length()-1;
+        while(si<=ei){
+
+            while(si<=ei && !Character.isLetterOrDigit(s.charAt(ei))){
+                ei--;
+            }
+
+            while(si<=ei && !Character.isLetterOrDigit(s.charAt(si))){
+                si++;
+            }
+
+            if(si<=ei && Character.toLowerCase(s.charAt(si))!=Character.toLowerCase(s.charAt(ei))){
+                return false;
+            }
+            si++;
+            ei--;
+        }
+        return true;
+    }
+}
+
